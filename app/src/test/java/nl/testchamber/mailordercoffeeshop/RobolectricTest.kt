0b0eebaaf398f5b1d1f5fb3a3a4301d9b5ddffad
Rutger Korstjens <rutger.korstjens@gmail.com>
2020-05-14 13:48:46 +0200

    create espresso instrumentation test PoC - Work in progress

--- app/src/test/java/nl/testchamber/mailordercoffeeshop/RobolectricTest.kt
+++ app/src/test/java/nl/testchamber/mailordercoffeeshop/RobolectricTest.kt
@@ -2,40 +2,53 @@ package nl.testchamber.mailordercoffeeshop
 
 import android.app.Application
 import android.content.Intent
+import android.os.Build
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers
 import org.hamcrest.core.AllOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+import org.robolectric.annotation.TextLayoutMode
 
-
-class EspressoActivityScenarioTest {
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+@TextLayoutMode(TextLayoutMode.Mode.REALISTIC)
+@LooperMode(LooperMode.Mode.PAUSED)
+class RobolectricTest {
 
     private lateinit var activityScenario: ActivityScenario<MainActivity>
 
     @Before
     fun setUp() {
-        val app = getApplicationContext<Application>()
+        val app = ApplicationProvider.getApplicationContext<Application>()
         SharedPreferencesUtil.setIsFirstLaunchToFalse(app.applicationContext)
+
         activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         Intents.init()
     }
 
     @After
-    fun tearDown(){
+    fun tearDown() {
         Intents.release()
         activityScenario.close()
     }
@@ -46,30 +59,31 @@ class EspressoActivityScenarioTest {
     @Test
     fun orderOverViewShouldDisplayIngredients() {
         Espresso.onView(ViewMatchers.withText("+")).perform(ViewActions.click(), ViewActions.click())
-        Espresso.onView(withId(R.id.chocolate)).perform(ViewActions.click())
-        Espresso.onView(withText(R.string.review_order_button)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.beverage_detail_ingredients)).check(ViewAssertions.matches(ViewMatchers.withText("Ingredients:\n2 shots of espresso\nChocolate")))
+        Espresso.onView(ViewMatchers.withId(R.id.chocolate)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.review_order_button)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.beverage_detail_ingredients)).check(ViewAssertions.matches(ViewMatchers.withText("Ingredients:\n2 shots of espresso\nChocolate")))
     }
 
     @Test
     fun shouldBeAbleToSelectAnItemInTheRecyclerView() {
-        Espresso.onView(withId(R.id.use_menu)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.beverage_recycler_view))
+        Espresso.onView(ViewMatchers.withId(R.id.use_menu)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.beverage_recycler_view))
                 .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(ViewMatchers.hasDescendant(ViewMatchers.withText("CON PANNA")), ViewActions.click()))
-        Espresso.onView(withId(R.id.beverage_detail_title)).check(ViewAssertions.matches(ViewMatchers.withText("Con Panna")))
+        Espresso.onView(ViewMatchers.withId(R.id.beverage_detail_title)).check(ViewAssertions.matches(ViewMatchers.withText("Con Panna")))
     }
 
     @Test
     fun shouldSendAnIntentContainingTheRightOrderName() {
+        onView(withId(R.id.espresso_shot_counter)).check(matches(withText("0")))
         Espresso.onView(ViewMatchers.withText("+")).perform(ViewActions.click(), ViewActions.click())
-        Espresso.onView(withId(R.id.chocolate)).perform(ViewActions.click())
-        Espresso.onView(withText(R.string.review_order_button)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.name_text_box)).perform(ViewActions.scrollTo(), ViewActions.typeText("My name"))
-        Espresso.onView(withId(R.id.custom_order_name_box)).perform(ViewActions.scrollTo(), ViewActions.typeText("Custom Order Name"))
-        Espresso.onView(withId(R.id.mail_order_button)).perform(ViewActions.scrollTo(), ViewActions.click())
+        onView(withId(R.id.espresso_shot_counter)).check(matches(withText("2")))
+        Espresso.onView(ViewMatchers.withId(R.id.chocolate)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.review_order_button)).perform(scrollTo(), ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.name_text_box)).perform(ViewActions.scrollTo(), ViewActions.typeText("My name"))
+        Espresso.onView(ViewMatchers.withId(R.id.custom_order_name_box)).perform(ViewActions.scrollTo(), ViewActions.typeText("Custom Order Name"))
+        Espresso.onView(ViewMatchers.withId(R.id.mail_order_button)).perform(ViewActions.scrollTo(), ViewActions.click())
         Intents.intended(AllOf.allOf(
                 IntentMatchers.hasAction(CoreMatchers.equalTo(Intent.ACTION_SENDTO)),
                 IntentMatchers.hasExtra(Intent.EXTRA_SUBJECT, "Order: My name - Custom Order Name")))
     }
-
 }
